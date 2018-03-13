a3593de8b1cfb396e70bca4a78b0a23fbb1dcb52
Philippe Luchansky <philuchansky@gmail.com>
2018-03-13 12:26:00 -0700

    add middleware

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const
   ejsLayouts = require('express-ejs-layouts'),
   PORT = 3000
 
+app.set('view engine', 'ejs')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(logger('dev'))
+app.use(ejsLayouts)
+
 app.listen(PORT, (err) => {
   console.log(err || `Server running on ${PORT}.`)
 })
